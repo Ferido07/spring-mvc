5f4495f5a91122e8bbd8ed3d50ee31df44fc416b
Ferid Zuber Oumer <feridyz@gmail.com>
2018-11-06 09:57:46 +0300

    version 1 on google app engine

--- src/main/java/com/fyzapps/springmvc/SpringMvcApplication.java
+++ src/main/java/com/fyzapps/springmvc/SpringMvcApplication.java
@@ -3,10 +3,18 @@ package com.fyzapps.springmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.*;
+
 @SpringBootApplication
+//@RestController
 public class SpringMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
+	/*
+  @GetMapping("/")
+  public String hello() {
+    return "hello world!";
+  }*/
 }
