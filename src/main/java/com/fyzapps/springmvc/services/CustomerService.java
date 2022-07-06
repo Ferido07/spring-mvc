ba601b27852c29c8faf3a765337e1ed218d57667
Ferido07 <feridyz@gmail.com>
2022-07-06 17:27:42 +0300

    Some changes i forgot about

--- src/main/java/com/fyzapps/springmvc/services/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.fyzapps.springmvc.services;
-
-import com.fyzapps.springmvc.domain.Customer;
-
-public interface CustomerService extends CRUDService<Customer>{
-
-}
--- src/main/java/com/fyzapps/springmvc/services/ProductService.java
+++
@@ -1,6 +0,0 @@
-package com.fyzapps.springmvc.services;
-
-import com.fyzapps.springmvc.domain.Product;
-
-public interface ProductService extends CRUDService<Product>{
-}
